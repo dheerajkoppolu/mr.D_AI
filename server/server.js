b82cf78f50bdef3beeae9289cb8830d1c57a7043
SAI DHEERAJ KOPPOLU <106304592+dheerajkoppolu@users.noreply.github.com>
2023-01-19 19:36:18 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,17 +3,36 @@ import * as dotenv from 'dotenv'
 import cors from 'cors'
 import { Configuration, OpenAIApi } from 'openai'
 
+ 
+
 dotenv.config()
+
+ 
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+const config = {
+  model: process.env.MODEL,
+  temperature: parseFloat(process.env.TEMPERATURE),
+  max_tokens: parseFloat(process.env.MAX_TOKENS),
+  top_p: parseFloat(process.env.TOP_P),
+  frequency_penalty: parseFloat(process.env.FREQUENCY_PENALTY),
+  presence_penalty: parseFloat(process.env.PRESENCE_PENALTY)
+}
+
+ 
+
 const openai = new OpenAIApi(configuration);
+
+ 
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+ 
 
 app.get('/', async (req, res) => {
   res.status(200).send({
@@ -21,28 +40,35 @@ app.get('/', async (req, res) => {
   })
 })
 
+ 
+
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
+ 
+
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      model: config.model,
+  prompt: `${prompt}`,
+  temperature: config.temperature,
+  max_tokens: config.max_tokens,
+  top_p: config.top_p,
+  frequency_penalty: config.frequency_penalty,
+  presence_penalty: config.presence_penalty
     });
+
+ 
 
     res.status(200).send({
       bot: response.data.choices[0].text
     });
+
+ 
 
   } catch (error) {
     console.error(error)
     res.status(500).send(error || 'Something went wrong');
   }
 })
-
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
